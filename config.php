331325ac6da90c82bb919f6cac86612c8a7701ea
voltan <djvoltan@gmail.com>
2015-06-05 17:15:56 +0430

    Import first version

---
+++ config.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Pi Engine (http://pialog.org)
+ *
+ * @link            http://code.pialog.org for the Pi Engine source repository
+ * @copyright       Copyright (c) Pi Engine http://pialog.org
+ * @license         http://pialog.org/license.txt BSD 3-Clause License
+ */
+return array(
+    // Version
+    'version'       => '0.0.1',
+    // Type of layouts available in the theme
+    'type'          => 'front',
+    // Title of the theme
+    'title'         => 'Cafeyab light Theme',
+    // Author information: name, email, website
+    'author'        => 'Architecture: Hossein Azizabadi <azizabadi@faragostaresh.com>;',
+    // Screenshot image, relative path in asset.
+    'screenshot'    => 'image/screenshot.png',
+    // License or theme images and scripts
+    'license'       => 'Creative Common License http://creativecommons.org/licenses/by/3.0/',
+    // Optional description
+    'description'   => 'Cafeyab website theme',
+    // Parent theme from which templates can be inherited, default as 'default'
+    'parent'        => '',
+    // Supported browsers
+    'browser'       => 'Internet Explorer: 9+; Chrome: 17+; Firefox: 10+; Safari: 5.1+; Opera: 9.8+;',
+    // List of custom front layouts, optional
+    'layout'        => array(
+        //'file-name'   => __('Layout name'),
+    ),
+);
